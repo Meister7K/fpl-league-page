1a4992e91dd275cf65448ef186dd17164ee4ae8d
Meister7K <kmanfinkel17@gmail.com>
2024-09-17 17:54:41 -0600

    new year league update

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "996177997512290304"; // your league ID
+export const leagueID = "1124846398970298368"; // your league ID
 export const leagueName = "Fantasy Premier League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
